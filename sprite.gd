477d221988edb714aa147d7d56c2f79266e70d65
evneis <evanneiswender@gmail.com>
2025-05-08 19:16:54 -0400

    correct signal implementation?

--- sprite.gd
+++ sprite.gd
@@ -1,4 +1,15 @@
 extends Sprite2D
+
+signal health_depleted(old_value, new_value)
+
+var health = 10
+
+func take_damage(amount):
+	var old_health = health
+	health -= amount
+	if health <= 0:
+		health_depleted.emit(old_health, health)
+
 var speed = 400
 var angular_speed = PI
 
